6c4a65f3c07762ef54b69010a1f4f1f2ab8d9207
Avii00723 <105117218+Avii00723@users.noreply.github.com>
2025-01-12 13:07:06 +0530

    initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,249 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weather/weather.dart';
+
+import 'const.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final WeatherFactory _wf = WeatherFactory(OPENWEATHER_API_KEY);
+  Weather? _weather;
+  final TextEditingController _cityController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather("Philadelphia"); // Default city
+  }
+
+  void _fetchWeather(String city) async {
+    try {
+      Weather weather = await _wf.currentWeatherByCityName(city);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print("Error fetching weather: $e");
+      // Optionally, show an error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error fetching weather for $city")),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          _searchBar(),
+          Expanded(
+            child: _buildUI(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _searchBar() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _cityController,
+              decoration: InputDecoration(
+                hintText: "Enter city name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                prefixIcon: const Icon(Icons.search),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          ElevatedButton(
+            onPressed: () {
+              if (_cityController.text.isNotEmpty) {
+                _fetchWeather(_cityController.text);
+              }
+            },
+            child: const Text("Search"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUI() {
+    if (_weather == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _locationHeader(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.08,
+          ),
+          _dateTimeInfo(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          _weatherIcon(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          _currentTemp(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          _extraInfo(),
+        ],
+      ),
+    );
+  }
+
+  Widget _locationHeader() {
+    return Text(
+      _weather?.areaName ?? "",
+      style: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _dateTimeInfo() {
+    DateTime now = _weather!.date!;
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              DateFormat("EEEE").format(now),
+              style: const TextStyle(
+                fontSize: 35,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Text(
+              DateFormat("d.M.y").format(now),
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _weatherIcon() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.20,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  "http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png"),
+              onError: (exception, stackTrace) {
+                print("Error loading weather icon: $exception");
+              },
+            ),
+          ),
+        ),
+        Text(
+          _weather?.weatherDescription ?? "",
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _currentTemp() {
+    return Text(
+      "${_weather?.temperature?.celsius?.toStringAsFixed(0)}°C",
+      style: const TextStyle(
+        color: Colors.black,
+        fontSize: 90,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _extraInfo() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.15,
+      width: MediaQuery.of(context).size.width * 0.80,
+      decoration: BoxDecoration(
+        color: Colors.deepPurple,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Max: ${_weather?.tempMax?.celsius?.toStringAsFixed(0)}°C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Min: ${_weather?.tempMin?.celsius?.toStringAsFixed(0)}°C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Wind: ${_weather?.windSpeed?.toStringAsFixed(0)} m/s",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Humidity: ${_weather?.humidity?.toStringAsFixed(0)}%",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
